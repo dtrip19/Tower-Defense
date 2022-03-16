d421b71d7d15f1689fb87f1f046ec92276ec70fa
dtrip19 <81382233+dtrip19@users.noreply.github.com>
2022-03-16 14:24:49 -0700

    Testing

---
+++ Scripts/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private float number;
+    private float maxNumber;
+
+    public int Number
+    {
+        get
+        {
+            return (int)number;
+        }
+        private set
+        {
+            number = Mathf.Clamp(value, 0, maxNumber);
+        }
+    }
+
+    private void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        GetComponent<Transform>();
+        transform.position = new Vector3(0, 1 + Mathf.Sin(Time.time), 0);
+    }
+}
