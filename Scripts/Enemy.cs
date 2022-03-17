2d51646705ccf16b784eed53b997334af810d75c
EddieFernandezSuit <eddie@lowkel.com>
2022-03-16 19:41:34 -0700

    Begin game tutorial

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Transform _transform;
+    public Transform Transform => _transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update() => print("me");
+
+
+}
---
+++ Scripts/Tower.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;
+    [SerializeField] float bulletSpeed;
+    Enemy target;
+    int timer = 50;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer++;
+        if (timer >= 100 && target != null)
+        {
+            timer = 0;
+            var bulletRb = Instantiate(bullet).GetComponent<Rigidbody>();
+            bulletRb.transform.position = transform.position;
+            var directionEnemy = target.Transform.position - transform.position;
+            bulletRb.velocity = directionEnemy.normalized * bulletSpeed;
+            Destroy(bulletRb.gameObject, 15);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Enemy enemy) && target == null)
+        {
+            target = enemy;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent(out Enemy enemy) && target != null && target.Equals(enemy))
+        {
+            
+            target = null;
+        }
+    }
+}
