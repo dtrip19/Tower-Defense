75758d06a0e362c482cb56c19c349d3ba5d8f4d0
Blastx6 <92227235+Blastx6@users.noreply.github.com>
2022-03-18 12:22:36 -0700

    RAycasting

---
+++ Scripts/TowerPlacementManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlacementManager : MonoBehaviour
+{
+
+    Camera camera;
+    GameObject ghostTower;
+    int layerMask = 1 << 3;
+    Transform ghostTowerTransform;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+        TowerSlot.OnSelect += CreateGhostTower;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ghostTower == null) return;
+        var ray = camera.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray,out RaycastHit hit, layerMask);
+        if (hit.point!=Vector3.zero){
+            ghostTowerTransform.position = hit.point;
+        }
+
+    }
+
+    void CreateGhostTower(GameObject ghostTower)
+    {
+        var newTower = Instantiate(ghostTower);
+        this.ghostTower = newTower;
+        ghostTowerTransform = newTower.transform;
+    }    
+
+    void OnDestroy()
+    {
+        TowerSlot.OnSelect -= CreateGhostTower;
+    }
+}
--- Scripts/TowerSlot.cs
+++ Scripts/TowerSlot.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-public class TowerSlot : MonoBehaviour
+using UnityEngine.EventSystems;
+using System;
+public class TowerSlot : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
 {
+
+    [SerializeField] GameObject tower;
+    public static event Action<GameObject> OnSelect;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,22 @@ public class TowerSlot : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+        OnSelect?.Invoke(tower);
+        print("clicked on the tower slot");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        print("mouse entered the tower slot image");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        print("mouse exit");
     }
 }
