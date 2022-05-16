de13c7a52aa221f555a03e3cd718799fe1be4af8
Blastx6 <92227235+Blastx6@users.noreply.github.com>
2022-05-15 23:32:55 -0700

    watermelon tower

--- Scripts/Towers/PlantTower/WatermelonTowerBehavior.cs
+++ Scripts/Towers/PlantTower/WatermelonTowerBehavior.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class WatermelonTowerBehavior : TowerBehaviorBase
 {
-    
+        protected override void Shoot()
+    {
+        var projectile = Instantiate(bullet).GetComponent<Projectile>();
+        projectile.Transform.position = BulletOrigin;
+        var dirToEnemy = target.LineOfSightPosition - BulletOrigin;
+
+        projectile.SetValues(target.transform.position, DamageType, Time.time + lifeTime, bulletSpeed, damage, pierce);
+    }
 }
--- Scripts/Towers/PlantTower/WatermelonTowerProjectile.cs
+++ Scripts/Towers/PlantTower/WatermelonTowerProjectile.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WatermelonTowerProjectile : MonoBehaviour
+public class WatermelonTowerProjectile : Projectile
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private Vector3 destination;
+    private float distToDestination;
+
+    private float gravity = -.4f;
+    private float vSpeed = 2;
+
+
+    new private void FixedUpdate()
     {
-        
+        base.FixedUpdate();
+        vSpeed += gravity;
+        _transform.position += new Vector3(0,vSpeed,0);
+    }
+    public override void SetValues(Vector3 destination, DamageType damageType, float timeDestroy, float speed, int damage, int pierce)
+    {
+        this.destination = destination;
+        this.damageType = damageType;
+        this.timeDestroy = timeDestroy;
+        this.damage = damage;
+        this.pierce = pierce;
+        this.distToDestination = Vector3.Distance(_transform.position, destination);
+        var dirToDestination = (destination - _transform.position).normalized;
+        direction = new Vector3(dirToDestination.x, 0, dirToDestination.z);
+        this.speed = distToDestination * speed /2;
+        // vSpeed = destination.y/10;
+
+        vSpeed =  speed + destination.y/10;
+        gravity = -speed * speed * .1f;
     }
 }
