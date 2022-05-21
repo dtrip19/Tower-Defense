e270b4424bf52ed7e0875b6e1ef358c0d3eb84ad
dtrip19 <81382233+dtrip19@users.noreply.github.com>
2022-05-20 22:18:02 -0700

    Tesla tower fix

--- Scripts/Towers/MagicTower/TeslaTowerBehavior.cs
+++ Scripts/Towers/MagicTower/TeslaTowerBehavior.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class TeslaTowerBehavior : TowerBehaviorBase
 {
-    new protected void Shoot()
+    protected override void Shoot()
     {
-        var colliders = Physics.OverlapSphere(_transform.position, range, (int)Layers.Enemy);
+        var colliders = Physics.OverlapSphere(_transform.position, range, Layers.Enemy);
 
         int enemiesShot = 0;
 
@@ -19,7 +19,7 @@ public class TeslaTowerBehavior : TowerBehaviorBase
             projectile.Transform.position = BulletOrigin;
             var dirToEnemy = enemy.LineOfSightPosition - BulletOrigin;
 
-            projectile.SetValues(dirToEnemy.normalized, DamageType.Normal, Time.time + lifeTime, bulletSpeed, damage, pierce);
+            projectile.SetValues(dirToEnemy.normalized, DamageType.Elemental, Time.time + lifeTime, bulletSpeed, damage, pierce);
             enemiesShot++;
         }
     }
