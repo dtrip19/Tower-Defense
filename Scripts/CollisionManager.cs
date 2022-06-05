85b6abbb8c2372ba972e2d5dc56add8952c2451b
dtrip19 <81382233+dtrip19@users.noreply.github.com>
2022-06-04 20:01:35 -0700

    Map 15 kinda

--- Scripts/CollisionManager.cs
+++ Scripts/CollisionManager.cs
@@ -1,12 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CollisionManager : MonoBehaviour
 {
     private void Awake()
     {
         Physics.IgnoreLayerCollision(Layers.ProjectileRaw, Layers.ProjectileRaw);
+        Physics.IgnoreLayerCollision(Layers.UnplaceableRaw, Layers.UnplaceableRaw);
+        Physics.IgnoreLayerCollision(Layers.UnplaceableRaw, Layers.GroundRaw);
+        Physics.IgnoreLayerCollision(Layers.GroundRaw, Layers.GroundRaw);
         //Physics.IgnoreLayerCollision(Layers.ProjectileRaw, 0);
     }
 }
